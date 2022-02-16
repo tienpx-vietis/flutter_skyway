73d5927c970b39c7753de3bf131f2c1318ca7e00
lamdt <lam.dt26042000@gmail.com>
2022-02-16 11:12:14 +0700

    add start_page

--- lib/presentation/home/home.view.dart
+++ lib/presentation/home/home.view.dart
@@ -8,12 +8,193 @@ class HomeView extends BaseView<HomeViewModel> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: SafeArea(
-        child: Center(
-            child: const Text("START")
-                .defaultStyle()
-                .fontSize(40)
-                .color(Colors.blue)),
+        child: Container(
+          padding: const EdgeInsets.only(top: 24, left: 24, right: 24),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                "Start Video Call",
+                style: TextStyle(
+                  fontFamily: FontFamily.roboto,
+                  fontStyle: FontStyle.normal,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500,
+                  color: Color(0xFF222222),
+                ),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              const Text(
+                "Start video call in mode: ",
+                style: TextStyle(
+                  fontFamily: FontFamily.roboto,
+                  fontWeight: FontWeight.w400,
+                  fontStyle: FontStyle.normal,
+                  fontSize: 15,
+                  color: Color(0xFF222222),
+                ),
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 5),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Expanded(
+                          flex: 1,
+                          child: SizedBox(
+                            height: 39,
+                            child: ElevatedButton(
+                              style: ButtonStyle(
+                                shape: MaterialStateProperty.all<
+                                    RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(7),
+                                  ),
+                                ),
+                                backgroundColor:
+                                    MaterialStateProperty.all<Color>(
+                                  const Color(0xFFF2F2F2),
+                                ),
+                              ),
+                              onPressed: viewModel.setSFUCallMode,
+                              child: const Text(
+                                "SFU",
+                                style: TextStyle(
+                                    fontFamily: FontFamily.roboto,
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 13,
+                                    color: Colors.black),
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        Expanded(
+                          flex: 1,
+                          child: SizedBox(
+                            height: 39,
+                            child: ElevatedButton(
+                              style: ButtonStyle(
+                                shape: MaterialStateProperty.all<
+                                    RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(7),
+                                  ),
+                                ),
+                                backgroundColor:
+                                    MaterialStateProperty.all<Color>(
+                                  const Color(0xFFE14D4D),
+                                ),
+                              ),
+                              onPressed: viewModel.setMESHCallMode,
+                              child: const Text(
+                                "MESH",
+                                style: TextStyle(
+                                    fontFamily: FontFamily.roboto,
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 13,
+                                    color: Colors.white),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 23,
+                    ),
+                    SizedBox(
+                      // width: 312,
+                      height: 40,
+                      child: Form(
+                        key: viewModel.formKey,
+                        autovalidateMode: AutovalidateMode.onUserInteraction,
+                        child: TextFormField(
+                          initialValue: viewModel.roomName,
+                          onChanged: (value) => viewModel.roomName = value,
+                          validator: viewModel.roomNameValidator,
+                          decoration: const InputDecoration(
+                            hintText: "Room Name",
+                            hintStyle: TextStyle(
+                                fontFamily: FontFamily.roboto,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w400,
+                                fontStyle: FontStyle.normal),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    SizedBox(
+                      width: double.infinity,
+                      height: 39,
+                      child: ElevatedButton(
+                        style: ButtonStyle(
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(7),
+                            ),
+                          ),
+                        ),
+                        onPressed: viewModel.showVideoChatView,
+                        child: const Text(
+                          "HOST",
+                          style: TextStyle(
+                            fontFamily: FontFamily.roboto,
+                            fontStyle: FontStyle.normal,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    SizedBox(
+                      width: double.infinity,
+                      height: 39,
+                      child: ElevatedButton(
+                        style: ButtonStyle(
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(7),
+                            ),
+                          ),
+                        ),
+                        onPressed: () {},
+                        child: const Text(
+                          "JOIN",
+                          style: TextStyle(
+                            fontFamily: FontFamily.roboto,
+                            fontStyle: FontStyle.normal,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/presentation/home/home.viewmodel.dart
+++ lib/presentation/home/home.viewmodel.dart
@@ -1,18 +1,49 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_skyway/core/base.dart';
+import 'package:get/get.dart';
 import 'package:mobx/mobx.dart';
+
+import '../app/app.pages.dart';
 
 part 'home.viewmodel.g.dart';
 
+enum CallModeType { sfu, mesh }
 class HomeViewModel = _HomeViewModel with _$HomeViewModel;
 
 abstract class _HomeViewModel extends BaseViewModel with Store {
-  @observable int value = 0;
+  final formKey = GlobalKey<FormState>();
+
+  bool get isFormValid => formKey.currentState?.validate() ?? false;
+
+  @observable
+  String roomName = "";
+
+  @observable
+  CallModeType callMode = CallModeType.sfu;
 
   @override
   void onInit() {
     super.onInit();
-    value = 1;
   }
 
-  @action increment() => value += 1;
+  @action
+  showVideoChatView() {
+    if (isFormValid) {
+      Get.toNamed(Routes.VIDEO_CHAT);
+    }
+  }
+
+  @action
+  setSFUCallMode() {
+    callMode = CallModeType.sfu;
+  }
+
+  @action
+  setMESHCallMode() {
+    callMode = CallModeType.mesh;
+  }
+
+  @action
+  String? roomNameValidator(String? value) =>
+      (value == null || value.isEmpty || value.length > 20) ? "Invalid" : null;
 }
